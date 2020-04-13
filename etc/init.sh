ad34dfefce3a7cd0b288d1e5561b5ba6a82a020a
Vladimir Turbin <vladimirturbin@gmail.com>
2020-04-13 17:04:48 +0300

    changes to init.sh, for sites-enabled/default deletion

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,4 @@
-sudo rm /etc/nginx/conf.d/*
+sudo rm /etc/nginx/conf.d/*.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/nginx.conf
 sudo service nginx restart
