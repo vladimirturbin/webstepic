9939100f042221a5022db07d3145910652e72b6d
Vladimir Turbin <vladimirturbin@gmail.com>
2020-04-15 19:59:53 +0300

    gunicorn service config added

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,10 @@
 sudo rm /etc/nginx/conf.d/*.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/nginx.conf
+
+sudo rm /etc/gunicorn.d/wsgi.example
+sudo rm /etc/gunicorn.d/django.example
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+
 sudo service nginx restart
