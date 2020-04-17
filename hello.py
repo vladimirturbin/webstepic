f194142ce4ba10475d7fe7a8bbcade95e38ebfb9
Vladimir Turbin <vladimirturbin@gmail.com>
2020-04-17 19:12:20 +0300

    remove debug output from hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 def application(env, start_response):
     query = env['QUERY_STRING']
     ret = bytes('\n'.join(query.split('&')), 'UTF8') + b'\n'
-    print('1')
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return iter([ret])
