0e404bbc17bc72d7f6cc4b8a9ba74f9027dbfed2
Vladimir Turbin <vladimirturbin@gmail.com>
2020-04-28 16:50:59 +0300

    hacks in models.py for step2_3_10 #4 test pass

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,11 +13,10 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
-    likes = models.ManyToManyField(User, related_name='liked_questions')
-
+    added_at = models.DateField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, blamk=True)
+    likes = models.ManyToManyField(User, related_name='liked_questions', blank=True)
     def __unicode__(self):
         return self.title
 
@@ -26,7 +25,7 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, blank=True)
 
     def __unicode__(self):
         return self.text
