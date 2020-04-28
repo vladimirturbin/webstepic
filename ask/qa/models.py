951bece4f8b9e8e7ea72b843d33258a121c022a2
vladimirturbin <Solderinirontip>
2020-04-28 13:08:55 +0300

    models for step2_3_10

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    ratings = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    likes = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __unicode__(self):
+        return self.text
+
