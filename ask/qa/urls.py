b7c931d72528720c5a07bbf2f7c55e042cae6e93
Vladimir Turbin <vladimirturbin@gmail.com>
2020-06-07 22:14:18 +0300

    week end

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_number>/', views.index, name='question')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:question_number>/', views.IndexView.as_view(), name='question')
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(requests, *args, **kwargs):
-    return HttpResponse("OK")
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        try:
+            page = int(request.GET.get('page', 1))
+        except TypeError:
+            page = 1
 
+        return HttpResponse(page)
